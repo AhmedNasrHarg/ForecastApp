65ec5b221a2d058ef1f028e18b5c76f9108d58db
Ahmed Nasr <ahmed.harg@yahoo.com>
2020-07-09 03:07:48 +0200

    forecast-init

---
+++ app/src/main/java/com/example/forecastapp/ui/settings/SettingsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.forecastapp.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.forecastapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return TextView(activity).apply {
+            setText(R.string.hello_blank_fragment)
+        }
+    }
+
+}
