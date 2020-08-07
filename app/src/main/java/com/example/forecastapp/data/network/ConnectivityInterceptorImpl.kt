c52de01e99401dea08b80563c92845a933c405ab
Ahmed Nasr <ahmed.harg@yahoo.com>
2020-08-07 23:29:08 +0200

    connection-interceptor

---
+++ app/src/main/java/com/example/forecastapp/data/network/ConnectivityInterceptorImpl.kt
@@ -0,0 +1,24 @@
+package com.example.forecastapp.data.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import com.example.forecastapp.internal.NoConnectivityExceptio
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+
+class ConnectivityInterceptorImpl(context:Context) : ConnectivityInterceptor {
+    private val appContext =context.applicationContext
+    override fun intercept(chain: Interceptor.Chain): Response {
+        if(!isOnline())
+            throw NoConnectivityExceptio()
+        return chain.proceed(chain.request())
+
+    }
+    private  fun isOnline():Boolean{
+        val connectivityManager = appContext.getSystemService(Context.CONNECTIVITY_SERVICE)
+        as ConnectivityManager
+        val netwokInfo = connectivityManager.activeNetworkInfo
+        return netwokInfo !=null && netwokInfo.isConnected
+    }
+}
